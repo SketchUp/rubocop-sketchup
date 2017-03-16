faf523ec9257c1f58e39396f2fb665677410836c
Thomas Thomassen <thomas@thomthom.net>
2017-03-16 17:50:34 +0100

    Adding spec for SketchupSuggestions/OperationName

--- lib/rubocop/sketchup/suggestions/operation_name.rb
+++ lib/rubocop/sketchup/suggestions/operation_name.rb
@@ -5,7 +5,7 @@ module RuboCop
     module SketchupSuggestions
       class OperationName < Cop
         MSG = 'Operation name should be a short capitalized description.'.freeze
-        
+
         def on_send(node)
           _, method_name, *args = *node
           return unless method_name == :start_operation
@@ -15,10 +15,16 @@ module RuboCop
         end
 
         def acceptable_operation_name?(name)
+          # Capitalization, no programmer name, no punctuation
           return false if name.size > 25 # TODO: Separate Cop?
           return false if name.end_with?('.')
-          # Capitalization, no programmer name, no punctuation
+          return false if titleize(name) != name
           true
+        end
+
+        # TODO(thomthom): Might need to ignore words like 'and/or'
+        def titleize(string)
+          string.split.map(&:capitalize).join(' ')
         end
       end
     end
---
+++ spec/rubocop/sketchup/suggestions/operation_name_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe RuboCop::Cop::SketchupSuggestions::OperationName do
+
+  subject(:cop) { described_class.new }
+
+  it 'registers an offense when operations name is too long' do
+    inspect_source(cop, 'model.start_operation("Some Really Long Operation Name")')
+    expect(cop.offenses.size).to eq(1)
+  end
+
+  it 'does not register an offense when operation name is short' do
+    inspect_source(cop, 'model.start_operation("Short")')
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'registers an offense when operations name ends with a period' do
+    inspect_source(cop, 'model.start_operation("Something.")')
+    expect(cop.offenses.size).to eq(1)
+  end
+
+  it 'registers an offense when operation name is not capitalized' do
+    inspect_source(cop, 'model.start_operation("doing stuff")')
+    expect(cop.offenses.size).to eq(1)
+  end
+
+  it 'does not register an offense operation when name is capitalized' do
+    inspect_source(cop, 'model.start_operation("Doing Stuff")')
+    expect(cop.offenses).to be_empty
+  end
+
+end
