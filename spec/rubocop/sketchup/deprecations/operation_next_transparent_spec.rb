08fc6a4e7ce10aa80461fd12660c56ed236115eb
Thomas Thomassen <thomas@thomthom.net>
2017-03-16 15:22:46 +0100

    Add spec for SketchupDeprecations/OperationNextTransparent

---
+++ spec/rubocop/sketchup/deprecations/operation_next_transparent_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe RuboCop::Cop::SketchupDeprecations::OperationNextTransparent do
+
+  subject(:cop) { described_class.new }
+
+  it 'registers an offense for start_operation third argument' do
+    inspect_source(cop, 'model.start_operation("Example", true, true)')
+    expect(cop.offenses.size).to eq(1)
+  end
+
+  it "does not register an offense for start_operation first argument" do
+    inspect_source(cop, 'model.start_operation("Example")')
+    expect(cop.offenses).to be_empty
+  end
+
+  it "does not register an offense for start_operation second argument" do
+    inspect_source(cop, 'model.start_operation("Example", true)')
+    expect(cop.offenses).to be_empty
+  end
+
+  it "does not register an offense for start_operation fourth argument" do
+    inspect_source(cop, 'model.start_operation("Example", true, false, true)')
+    expect(cop.offenses).to be_empty
+  end
+  
+end
