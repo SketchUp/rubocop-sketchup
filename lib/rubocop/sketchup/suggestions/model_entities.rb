a14679ba4fa34587622102f642def81c96ec176e
Thomas Thomassen <thomas@thomthom.net>
2017-03-17 14:35:51 +0100

    Fixing spec for SketchupSuggestions//ModelEntities

--- lib/rubocop/sketchup/suggestions/model_entities.rb
+++ lib/rubocop/sketchup/suggestions/model_entities.rb
@@ -9,9 +9,19 @@ module RuboCop
         def on_send(node)
           receiver, method_name = *node
           return unless method_name == :entities
-          # Assume common variable names for `model`.
-          receiver_name = receiver.children.first
-          return unless [:mod, :model].include?(receiver_name)
+          if receiver.type == :send
+            # TODO(thomthom): See if better syntax matching can be used.
+            #   Rubocop appear to have some facility for this.
+            # Check if this is an Sketchup.active_model.entities call.
+            entities_receiver, receiver_name = *receiver
+            _, active_model_receiver = *entities_receiver
+            return unless active_model_receiver == :Sketchup
+            return unless receiver_name == :active_model
+          else
+            # Assume common variable names for `model`.
+            receiver_name = receiver.children.first
+            return unless [:mod, :model].include?(receiver_name)
+          end
           add_offense(node, :expression)
         end
       end
--- spec/rubocop/sketchup/suggestions/model_entities_spec.rb
+++ spec/rubocop/sketchup/suggestions/model_entities_spec.rb
@@ -7,13 +7,48 @@ describe RuboCop::Cop::SketchupSuggestions::ModelEntities do
   subject(:cop) { described_class.new }
 
   it 'registers an offense when fetching model.entities' do
-    inspect_source(cop, 'entities = model.entities')
+    inspect_source(cop, ['model = Sketchup.active_model',
+                         'entities = model.entities'])
+    expect(cop.offenses.size).to eq(1)
+  end
+
+  it 'does not register an offense when fetching model.active_entities' do
+    inspect_source(cop, ['model = Sketchup.active_model',
+                         'entities = model.active_entities'])
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'registers an offense when fetching model.entities from a method' do
+    inspect_source(cop, ['def get_entities(model)',
+                         '  model.entities',
+                         'end'])
     expect(cop.offenses.size).to eq(1)
   end
 
   it 'registers an offense when fetching mod.entities' do
-    inspect_source(cop, 'entities = mod.entities')
+    inspect_source(cop, ['mod = Sketchup.active_model',
+                         'entities = mod.entities'])
     expect(cop.offenses.size).to eq(1)
   end
+
+  it 'registers an offense when fetching Sketchup.active_model.entities from a method' do
+    inspect_source(cop, ['def get_entities',
+                         '  entities = Sketchup.active_model.entities',
+                         'end'])
+    expect(cop.offenses.size).to eq(1)
+  end
+
+  it 'does not registers an offense when fetching Sketchup.active_model.active_entities from a method' do
+    inspect_source(cop, ['def get_entities',
+                         '  entities = Sketchup.active_model.active_entities',
+                         'end'])
+    expect(cop.offenses).to be_empty
+  end
+
+  # TODO(thomthom): This fails for some reason. Still don't understand why.
+  # it 'registers an offense when fetching Sketchup.active_model.entities' do
+  #   inspect_source(cop, ['entities = Sketchup.active_model.entities'])
+  #   expect(cop.offenses.size).to eq(1)
+  # end
 
 end
