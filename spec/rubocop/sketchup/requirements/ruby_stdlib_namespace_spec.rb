d5f8c915e26909699adf262e52109d62a0357a31
Thomas Thomassen <thomas@thomthom.net>
2017-03-21 15:06:22 +0100

    Adding cop SketchupRequirements/RubyStdLibNamespace

---
+++ spec/rubocop/sketchup/requirements/ruby_stdlib_namespace_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe RuboCop::Cop::SketchupRequirements::RubyStdLibNamespace do
+
+  subject(:cop) { described_class.new }
+
+  described_class::NAMESPACES_RUBY_STDLIB.each do |namespace|
+
+    type, var = namespace.split(' ')
+
+    it "registers an offense for adding an instance method to #{var} #{type}" do
+      inspect_source(cop, ["#{type} #{var}",
+                          '  def example',
+                          '  end',
+                          'end'])
+      expect(cop.offenses.size).to eq(1)
+    end
+
+    it "registers an offense for adding a module method to #{var} #{type}" do
+      inspect_source(cop, ["#{type} #{var}",
+                          '  def self.example',
+                          '  end',
+                          'end'])
+      expect(cop.offenses.size).to eq(1)
+    end
+
+    it "registers an offense for adding a constant to #{var} #{type}" do
+      inspect_source(cop, ["#{type} #{var}",
+                          '  EXAMPLE = 123',
+                          'end'])
+      expect(cop.offenses.size).to eq(1)
+    end
+
+    it "registers an offense for adding a module to #{var} #{type}" do
+      inspect_source(cop, ["#{type} #{var}",
+                          '  module Example',
+                          '  end',
+                          'end'])
+      expect(cop.offenses.size).to eq(1)
+    end
+
+    it "registers an offense for adding a class to #{var} #{type}" do
+      inspect_source(cop, ["#{type} #{var}",
+                          '  class Example',
+                          '  end',
+                          'end'])
+      expect(cop.offenses.size).to eq(1)
+    end
+  end
+
+end
---
+++ tools/stdlib_scraper.rb
@@ -0,0 +1,93 @@
+require 'nokogiri'
+require 'pathname'
+require 'set'
+
+require 'rubocop'
+require 'rubocop-sketchup'
+RUBY_CORE_NAMESPACES = Set.new(RuboCop::Cop::SketchupRequirements::RubyCoreNamespace::NAMESPACES)
+
+# Folder with downloaded StdLib documentation. (Extracted from Zips)
+# http://ruby-doc.org/
+# 
+# 1. ruby_1_8_6_stdlib_rdocs.tgz
+# 2. ruby_2_0_0_stdlib_rdocs.tgz
+# 3. ruby_2_2_6_stdlib_rdocs.tgz
+# 4. Unpack each archive so you have:
+#    * ruby_1_8_6_stdlib
+#    * ruby_2_0_0_stdlib
+#    * ruby_2_2_6_stdlib
+# 5. Update the path below and run script.
+# 6. Take the generated output and merge into:
+#    lib/rubocop/sketchup/requirements/ruby_std_namespace.rb
+source_path = 'C:/Users/tthomas2/Downloads/RubyDocs'
+puts source_path
+
+ruby_versions = Pathname.new(source_path).children.select(&:directory?)
+
+def extract_classes(file)
+  page = Nokogiri::HTML(open(file))
+  items = page.css('#class-index .entries p')
+  # items.map(&:text)
+  items.map { |item|
+    {
+      type: item['class'],
+      name: item.css('a').text
+    }
+  }
+end
+
+ruby_versions.each { |path|
+  libdoc_path = path.join('libdoc')
+
+  top_level_namespaces = {
+    :class => Set.new,
+    :module => Set.new
+  }
+
+  modules = libdoc_path.children.select(&:directory?)
+  modules.each { |module_path|
+    # Ignore noisy tk library
+    next if module_path.basename.to_s.downcase == 'tk'
+
+    pattern = module_path.join('**/rdoc/index.html')
+    files = Pathname.glob(pattern)
+
+    files.each { |index_file|
+      items = extract_classes(index_file)
+      # Get the top level namespace of the objects found.
+      top_level_items = items.map { |item|
+        name = item[:name].split('::').find { |n| n != 'Object' }.to_s
+        {
+          type: item[:type].split(' ').first.intern,
+          name: name
+        }
+      }
+      # Filter out anything indicate nodoc.
+      # top_level_items.reject! { |item| item[:type].include?('nodoc') }
+      # Filter out anything which doesn't start with a capital letter.
+      top_level_items.select! { |item| /\A[[:upper:]]/.match(item[:name]) }
+      # Filter out odd objects with arrow (→) in their names.  →
+      top_level_items.reject! { |item| item[:name].include?("\u2192") }
+      # Filter out Ruby Core namespaces.
+      top_level_items.reject! { |item|
+        RUBY_CORE_NAMESPACES.include?(item[:name])
+      }
+      # The remaining items should be the relevant top level namespaces.
+      top_level_items.each { |item|
+        type = item[:type]
+        top_level_namespaces[type] << item[:name]
+      }
+    }
+  }
+  
+  # Output a Ruby array with the namespaces.
+  lib_name = path.basename.to_s.upcase
+  puts "NAMESPACES_#{lib_name} = ["
+  top_level_namespaces.each { |type, names|
+    names.each { |name|
+      puts %(  "#{type} #{name}",)
+    }
+  }
+  puts "].freeze"
+  puts
+}
