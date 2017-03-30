a69246c613616d5e5aecc6a6d0ec058ce0a5f3fa
Thomas Thomassen <thomas@thomthom.net>
2017-03-30 14:30:52 +0200

    Improve wording in ExtensionNamespace cop.

--- lib/rubocop/sketchup/requirements/extension_namespace.rb
+++ lib/rubocop/sketchup/requirements/extension_namespace.rb
@@ -53,7 +53,7 @@ module RuboCop
 
         def message(node)
           namespace = Namespace.new(node.defined_module_name).from_root
-          format('Use a single root namespace. (Expected %s; found %s)', @@namespace, namespace)
+          format('Use a single root namespace. (Found %s; Previously found %s)', namespace, @@namespace)
         end
 
       end
