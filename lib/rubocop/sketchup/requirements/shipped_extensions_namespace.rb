43e660876938cf550ce923cd4ad1842df5486fe8
Thomas Thomassen <thomas@thomthom.net>
2017-11-21 13:55:12 +0100

    Cop: Shipped Extensions Namespace. Fixes #11

---
+++ lib/rubocop/sketchup/requirements/shipped_extensions_namespace.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module SketchupRequirements
+      # Don't modify SketchUp's shipped extensions.
+      class ShippedExtensionsNamespace < Cop
+
+        include NoCommentDisable
+        include Sketchup::NamespaceChecker
+
+        MSG = 'Do not modify shipped extensions.'.freeze
+
+        # We check only against the top level namespaces. The core define more
+        # objects, but they are under one of the top level namespaces listed.
+
+        NAMESPACES_ADVANCED_CAMERA_TOOLS = %w(
+          ACTUtil
+          CameraAppObserver
+          CameraEntityObserver
+          CameraFrameChangeObserver
+          CameraRep
+          CameraToolModelObserver
+          CameraToolPagesObserver
+          CameraToolViewObserver
+          FilmCameraTool
+          FSCameraData
+          FSGeomUtils
+          FSValidate
+          PageNameChangeObserver
+        ).freeze
+
+        NAMESPACES_DYNAMIC_COMPONENTS = %w(
+          DCConverter
+          DCDownloader
+          DCFunctionsV1
+          DCInteractTool
+          DCObservers
+          DCProgressBar
+          DynamicComponents
+          DynamicComponentsV1
+        ).freeze
+
+        NAMESPACES_TRIMBLE_CONNECT = %w(
+          Trimble
+        ).freeze
+
+        NAMESPACES = (
+          NAMESPACES_ADVANCED_CAMERA_TOOLS |
+          NAMESPACES_DYNAMIC_COMPONENTS |
+          NAMESPACES_TRIMBLE_CONNECT
+        ).freeze
+
+        def namespaces
+          NAMESPACES
+        end
+
+      end
+    end
+  end
+end
---
+++ spec/rubocop/sketchup/requirements/shipped_extensions_namespace_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe RuboCop::Cop::SketchupRequirements::ShippedExtensionsNamespace do
+
+  subject(:cop) { described_class.new }
+
+  described_class::NAMESPACES.each do |var|
+
+    # The namespace objects are either modules or classes, but we don't know
+    # what. We'll try to infer this based on the current Ruby installation
+    # running the tests, defaulting back to `module`.
+    # Picking the correct type isn't really that important, but we'll try in
+    # order to keep as close to what they represent as possible.
+    type = 'module'
+    if Object.constants.include?(var.intern)
+      const = Object.const_get(var)
+      type = const.class.name.downcase
+      next unless %w(class module).include?(type)
+    end
+
+    it "registers an offense for adding an instance method to #{var} #{type}" do
+      inspect_source(["#{type} #{var}",
+                      '  def example',
+                      '  end',
+                      'end'])
+      expect(cop.offenses.size).to eq(1)
+    end
+
+    it "registers an offense for adding a module method to #{var} #{type}" do
+      inspect_source(["#{type} #{var}",
+                      '  def self.example',
+                      '  end',
+                      'end'])
+      expect(cop.offenses.size).to eq(1)
+    end
+
+    it "registers an offense for adding a constant to #{var} #{type}" do
+      inspect_source(["#{type} #{var}",
+                      '  EXAMPLE = 123',
+                      'end'])
+      expect(cop.offenses.size).to eq(1)
+    end
+
+    it "registers an offense for adding a module to #{var} #{type}" do
+      inspect_source(["#{type} #{var}",
+                      '  module Example',
+                      '  end',
+                      'end'])
+      expect(cop.offenses.size).to eq(1)
+    end
+
+    it "registers an offense for adding a class to #{var} #{type}" do
+      inspect_source(["#{type} #{var}",
+                      '  class Example',
+                      '  end',
+                      'end'])
+      expect(cop.offenses.size).to eq(1)
+    end
+
+    it 'does not register an offense for namespaced objects' do
+    inspect_source(['module Example',
+                    'end'])
+    expect(cop.offenses).to be_empty
+  end
+  end
+
+end
