92396a9bb0e8d7ba92af38e6565cddd1b2562291
Thomas Thomassen <thomas@thomthom.net>
2017-12-14 18:39:16 +0100

    Base for custom formatter.

--- lib/rubocop-sketchup.rb
+++ lib/rubocop-sketchup.rb
@@ -3,6 +3,7 @@ require 'rubocop/sketchup'
 require 'rubocop/sketchup/version'
 require 'rubocop/sketchup/inject'
 
+require 'rubocop/sketchup/formatter/extension_review'
 require 'rubocop/sketchup/extension_project'
 require 'rubocop/sketchup/namespace'
 require 'rubocop/sketchup/namespace_checker'
