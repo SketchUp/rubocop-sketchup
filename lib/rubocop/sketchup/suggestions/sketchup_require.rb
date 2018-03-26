156a1489a4d2c1c31e426bfe2509d81fa54f5126
Thomas Thomassen <thomas@thomthom.net>
2018-03-26 12:37:31 +0200

    Fix incorrect node pattern in SketchupRequire.

--- lib/rubocop/sketchup/suggestions/sketchup_require.rb
+++ lib/rubocop/sketchup/suggestions/sketchup_require.rb
@@ -18,14 +18,12 @@ module RuboCop
           )
         PATTERN
 
-        # Temporarily disabled due to RuboCop bug:
-        # https://github.com/bbatsov/rubocop/issues/5656
-        # def_node_matcher :sketchup_require?, <<-PATTERN
-        #   (send
-        #     (const nil? :Sketchup) :require
-        #     (str _)
-        #   )
-        # PATTERN
+        def_node_matcher :sketchup_require?, <<-PATTERN
+          (send
+            (const nil? :Sketchup) :require
+            (str _)
+          )
+        PATTERN
 
 
         def_node_matcher :sketchup_extension_new, <<-PATTERN
@@ -35,14 +33,12 @@ module RuboCop
             (str $_))
         PATTERN
 
-        # Temporarily disabled due to RuboCop bug:
-        # https://github.com/bbatsov/rubocop/issues/5656
-        # def_node_matcher :sketchup_extension_new?, <<-PATTERN
-        #   (:send
-        #     (:const nil? :SketchupExtension) :new
-        #     _
-        #     (str _))
-        # PATTERN
+        def_node_matcher :sketchup_extension_new?, <<-PATTERN
+          (send
+            (const nil? :SketchupExtension) :new
+            _
+            (str _))
+        PATTERN
 
 
         def on_send(node)
@@ -60,18 +56,6 @@ module RuboCop
 
         def valid_filename?(filename)
           File.extname(filename).empty?
-        end
-
-        # Workaround for RuboCop bug:
-        # https://github.com/bbatsov/rubocop/issues/5656
-        def sketchup_require?(node)
-          node.method_name == :require && sketchup_require(node)
-        end
-
-        # Workaround for RuboCop bug:
-        # https://github.com/bbatsov/rubocop/issues/5656
-        def sketchup_extension_new?(node)
-          node.method_name == :new && sketchup_extension_new(node)
         end
 
       end
