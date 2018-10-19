9226b2b6e3ed7b65633459bffadd4a268379110a
Thomas Thomassen <thomas@thomthom.net>
2018-10-19 16:32:00 +0200

    Add trailing commas.

--- lib/rubocop/sketchup/dc_methods.rb
+++ lib/rubocop/sketchup/dc_methods.rb
@@ -7,121 +7,121 @@ module RuboCop
       DC_METHODS = [
         {
           name: :rotx,
-          path: 'Geom::Transformation'
+          path: 'Geom::Transformation',
         },
         {
           name: :roty,
-          path: 'Geom::Transformation'
+          path: 'Geom::Transformation',
         },
         {
           name: :rotz,
-          path: 'Geom::Transformation'
+          path: 'Geom::Transformation',
         },
         {
           name: :xscale,
-          path: 'Geom::Transformation'
+          path: 'Geom::Transformation',
         },
         {
           name: :yscale,
-          path: 'Geom::Transformation'
+          path: 'Geom::Transformation',
         },
         {
           name: :zscale,
-          path: 'Geom::Transformation'
+          path: 'Geom::Transformation',
         },
 
         {
           name: :copy,
           path: 'Sketchup::Camera',
-          variables: [:camera, :cam]
+          variables: [:camera, :cam],
         },
         {
           name: :update,
           path: 'Sketchup::Camera',
-          variables: [:camera, :cam]
+          variables: [:camera, :cam],
         },
         {
           name: :same_camera_params?,
-          path: 'Sketchup::Camera'
+          path: 'Sketchup::Camera',
         },
 
         {
           name: :copy,
           path: 'Sketchup::ComponentInstance',
-          variables: [:instance, :inst]
+          variables: [:instance, :inst],
         },
         {
           name: :description,
           path: 'Sketchup::ComponentInstance',
-          variables: [:instance, :inst]
+          variables: [:instance, :inst],
         },
 
         {
           name: :local_transformation,
-          path: 'Sketchup::Drawingelement'
+          path: 'Sketchup::Drawingelement',
         },
         {
           name: :scaled_size,
-          path: 'Sketchup::Drawingelement'
+          path: 'Sketchup::Drawingelement',
         },
         {
           name: :unscaled_size,
-          path: 'Sketchup::Drawingelement'
+          path: 'Sketchup::Drawingelement',
         },
         {
           name: :set_last_size,
-          path: 'Sketchup::Drawingelement'
+          path: 'Sketchup::Drawingelement',
         },
         {
           name: :last_scaling_factors,
-          path: 'Sketchup::Drawingelement'
+          path: 'Sketchup::Drawingelement',
         },
 
         {
           name: :get_attributes,
-          path: 'Sketchup::Entity'
+          path: 'Sketchup::Entity',
         },
         {
           name: :has_attributes?,
-          path: 'Sketchup::Entity'
+          path: 'Sketchup::Entity',
         },
 
         {
           name: :typename,
           path: 'Sketchup::Model',
-          variables: [:model, :mod]
+          variables: [:model, :mod],
         },
         {
           name: :entityID,
           path: 'Sketchup::Model',
-          variables: [:model, :mod]
+          variables: [:model, :mod],
         },
         {
           name: :delete_attribute,
           path: 'Sketchup::Model',
-          variables: [:model, :mod]
+          variables: [:model, :mod],
         },
         {
           name: :layer,
           path: 'Sketchup::Model',
-          variables: [:model, :mod]
+          variables: [:model, :mod],
         },
 
         {
           name: :last_width=,
-          path: 'UI::WebDialog'
+          path: 'UI::WebDialog',
         },
         {
           name: :last_height=,
-          path: 'UI::WebDialog'
+          path: 'UI::WebDialog',
         },
         {
           name: :last_width,
-          path: 'UI::WebDialog'
+          path: 'UI::WebDialog',
         },
         {
           name: :last_height,
-          path: 'UI::WebDialog'
+          path: 'UI::WebDialog',
         },
       ]
 
