d4d1d5850702131389d3796673210d04a5279c43
Thomas Thomassen <thomas@thomthom.net>
2022-03-02 18:42:50 +0100

    Catch modifications of core API classes/modules via include/extend. Fixes #151

--- lib/rubocop/sketchup/namespace_checker.rb
+++ lib/rubocop/sketchup/namespace_checker.rb
@@ -4,6 +4,35 @@ module RuboCop
   module SketchUp
     module NamespaceChecker
 
+      extend NodePattern::Macros
+
+      # Example:
+      #   Array.include(Example)
+      def_node_matcher :klass_include, <<-PATTERN
+        (send
+          (const nil? $_) { :include | :extend } _)
+      PATTERN
+
+      # Example:
+      #   class Array
+      #     include Example
+      #   end
+      def_node_matcher :klass_scope_include, <<-PATTERN
+        (
+          {
+            class (const nil? $_) nil? |
+            module (const nil? $_)
+          }
+          (send nil? { :include | :extend } _))
+      PATTERN
+
+      # Example:
+      #   include Example
+      def_node_matcher :include_call?, <<-PATTERN
+        (send nil? { :include | :extend } _)
+      PATTERN
+
+
       def on_class(node)
         check_namespace(node)
       end
@@ -22,10 +51,38 @@ module RuboCop
         check_namespace(node)
       end
 
+      def on_send(node)
+        klass = klass_include(node) || klass_scope_include(node.parent)
+
+        if klass
+          namespace = SketchUp::Namespace.new(klass.to_s)
+          return unless namespaces.include?(namespace.first)
+        else
+          # If klass is `nil` we either don't have a `include` call or we're in
+          # global namespace.
+          return unless top_level_include?(node)
+        end
+
+        add_offense(node, location: :selector, severity: :error)
+      end
+
       def check_namespace(node)
         return unless in_namespace?(node)
 
         add_offense(node, location: :name, severity: :error)
+      end
+
+      def top_level?(node)
+        # parent_module_name might return nil if for instance a method is
+        # defined within a block. (Apparently that is possible...)
+        return false if node.parent_module_name.nil?
+
+        namespace = SketchUp::Namespace.new(node.parent_module_name)
+        namespace.top_level?
+      end
+
+      def top_level_include?(node)
+        include_call?(node) && top_level?(node)
       end
 
       def in_namespace?(node)
--- spec/rubocop/sketchup/cop/requirements/ruby_core_namespace_spec.rb
+++ spec/rubocop/sketchup/cop/requirements/ruby_core_namespace_spec.rb
@@ -5,10 +5,12 @@ require 'spec_helper'
 describe RuboCop::Cop::SketchupRequirements::RubyCoreNamespace do
 
   self::NAMESPACE_TYPES = %w[class module].freeze
+  self::MUTATORS = %w[include extend].freeze
 
   subject(:cop) { described_class.new }
 
-  described_class::NAMESPACES.each do |var|
+  # described_class::NAMESPACES.each do |var|
+  %w[Array Math].each do |var|
 
     # The namespace objects are either modules or classes, but we don't know
     # what. We'll try to infer this based on the current Ruby installation
@@ -74,6 +76,69 @@ describe RuboCop::Cop::SketchupRequirements::RubyCoreNamespace do
     it 'does not register an offense for namespaced objects' do
       expect_no_offenses(<<~RUBY)
         module Example
+        end
+      RUBY
+    end
+
+    self::MUTATORS.each do |method|
+
+      it "registers an offense for using #{method} on the #{var} #{type}" do
+        expect_offense(<<~RUBY, method: method)
+          module Example; end
+          #{type} #{var}
+            %{method} Example
+            ^{method} Do not modify Ruby core functionality.
+          end
+        RUBY
+      end
+
+      it "does not register an offense using #{method} in namespaced scope" do
+        expect_no_offenses(<<~RUBY)
+          module World; end
+          module Example
+            #{method} World
+          end
+        RUBY
+      end
+
+      it "registers an offense using #{method} on the #{var} #{type} via klass.#{method}" do
+        expect_offense(<<~RUBY, var: var, method: method)
+          module Example; end
+          %{var}.%{method} Example
+          _{var} ^{method} Do not modify Ruby core functionality.
+        RUBY
+      end
+
+      it "does not register an offense for using #{method} on namespaced objects" do
+        expect_no_offenses(<<~RUBY)
+          module Example; end
+          module World; end
+          Example.#{method}(World)
+        RUBY
+      end
+    end
+  end
+
+  self::MUTATORS.each do |method|
+
+    it "registers an offense for using #{method} in global scope" do
+      expect_offense(<<~RUBY, method: method)
+        module Example; end
+        %{method} Example
+        ^{method} Do not modify Ruby core functionality.
+      RUBY
+    end
+
+    it 'does not register an offense for calling another method in global scope' do
+      expect_no_offenses(<<~RUBY)
+        system('notepad')
+      RUBY
+    end
+
+    it 'does not register an offense for calling another method in a module scope' do
+      expect_no_offenses(<<~RUBY)
+        def Example
+          system('notepad')
         end
       RUBY
     end
